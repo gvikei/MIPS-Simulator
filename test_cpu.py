af1e445ace89f7889c2c54cd7c509ba1ceb73394
gvikei <truckhuyen@gmail.com>
2015-12-29 11:33:10 +0700

    Added custom input file

--- test_cpu.py
+++ test_cpu.py
@@ -1,14 +1,29 @@
 '''
 Created on Oct 26, 2014
+Updated on Dec 29, 2015
 
 @author: khuyenduong
-'''
-import cpu
 
-def main():
-    CPU = cpu.CPU()
-    CPU._LoadInstructions('count.txt')
-    CPU.ExecuteCPU()
+How to run: python test_cpu.py [instruction_file]
+E.g.: python test_cpu.py fizzbuzz.txt
+'''
+import cpu, sys, os
+
+def file_input():
+    if (len(sys.argv) < 2):
+        print "Please add file name to the argument as the following:"
+        print "python test_cpu.py [instruction_file]"
+        print "E.g.: python test_cpu.py testFiles/fizzbuzz.txt"
+        sys.exit()
+
+    instruction_file = sys.argv[1]
+    while (not os.path.isfile(instruction_file)):
+        instruction_file = raw_input("File name was invalid. Please try another one: ")
+            
+    return instruction_file
 
 if __name__ == "__main__":
-    main()
+    # reading and executing instruction file
+    CPU = cpu.CPU()
+    CPU._LoadInstructions(file_input())
+    CPU.ExecuteCPU()
